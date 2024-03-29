15b152292cc15e41159b170dc209283d8b8af938
kenikimchi <kb8370@gmail.com>
2024-03-29 00:04:11 -1000

    Modified apigateway role for cloudwatch

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -36,3 +36,50 @@ resource "aws_apigatewayv2_route" "lambda_route" {
   route_key = "POST /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
 }
+
+# Permissions
+resource "aws_api_gateway_account" "cwlogs_permissions" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch.arn
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+
+    actions = [ "sts:AssumeRole" ]
+  }
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  name = "api_gateway_cloudwatch"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "cloudwatch" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:FilterLogEvents"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name = "default"
+  role = aws_iam_role.cloudwatch.id
+  policy = data.aws_iam_policy_document.cloudwatch.json
+}
